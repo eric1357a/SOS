3b41f47f9d311d4deb4978fa547fd4a6d510a15b
‏ <nodegin@gmail.com>
2015-11-20 13:11:09 +0800

    fuck

---
+++ src/java/sos/tag/SearchResultTag.java
@@ -0,0 +1,28 @@
+package sos.tag;
+
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class SearchResultTag extends SimpleTagSupport {
+  
+  private int id;
+  private String title, desc;
+  
+  public int getId () { return id; }
+  public void setId (int i) { id = i; }
+  public String getTitle () { return title; }
+  public void setTitle (String t) { title = t; }
+  public String getDesc () { return desc; }
+  public void setDesc (String d) { desc = d; }
+  
+  public void doTag() throws java.io.IOException {
+    JspWriter out = getJspContext().getOut();
+    out.print("<a class='result' href='item?action=details&id=10101'>");
+    out.print("  <div class='content'>");
+    out.print("    <div class='title'>" + title + "</div>");
+    out.print("    <div class='description'>" + desc + "</div>");
+    out.print("  </div>");
+    out.print("</a>");
+  }
+  
+}
--- web/item/searchResult.jsp
+++ web/item/searchResult.jsp
@@ -1,14 +1,9 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@taglib uri="/WEB-INF/tlds/search-result" prefix="sos"%>
 <div>
   <div class="category">
     <div class="name">Pen</div>
-    <a class="result" href="item?action=details&id=10101">
-      <div class="content">
-        <div class="title">Coupe&trade;</div>
-        <div class="description">
-          ent will register their information (name, telephone number, and delivery address). After registrati...
-        </div>
-      </div>
-    </a>
+    <sos:searchResult id="1" title="<%="Coupe&trade;"%>" desc="fuck"/>
+    <sos:searchResult id="2" title="<%="Vault penis;"%>" desc="fuck"/>
   </div>
 </div>
